e90d576901953f86aa43903bf31fc21cfa60c016
nikymorg <nicole.morgan@gmail.com>
2018-09-12 09:59:27 -0400

    separate edit component

--- src/components/CourseContainer.js
+++ src/components/CourseContainer.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
-import CourseSelector from "./CourseSelector";
-import StudentsList from "./StudentsList";
+import React, { Component } from "react"
+import CourseSelector from "./CourseSelector"
+import EditStudent from "./EditStudent"
+import StudentsList from "./StudentsList"
 
 class CourseContainer extends Component {
   state = {
@@ -20,46 +21,12 @@ class CourseContainer extends Component {
 
         <CourseSelector courseList={[]} />
 
-        {/* Edit Form */}
-        <form
-          className="ui form center aligned sixteen wide column"
-          onSubmit={""}
-        >
-          <div className="inline fields">
-            <div className="four wide field">
-              <input
-                id="name"
-                type="text"
-                value={""}
-                onChange={"your code here"}
-              />
-            </div>
-            <div className="four wide field">
-              <input
-                id="class_year"
-                type="number"
-                value={""}
-                onChange={"your code here"}
-              />
-            </div>
-            <div className="four wide field">
-              <input
-                id="percentage"
-                type="number"
-                value={""}
-                onChange={"your code here"}
-              />
-            </div>
-            <button className="ui button" type="submit">
-              Submit
-            </button>
-          </div>
-        </form>
+        <EditStudent />
 
         <StudentsList />
       </div>
-    );
+    )
   }
 }
 
-export default CourseContainer;
+export default CourseContainer
---
+++ src/components/EditStudent.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Student = () => {
+
+  return (
+    <form
+      className="ui form center aligned sixteen wide column"
+      onSubmit={""}
+    >
+      <div className="inline fields">
+        <div className="four wide field">
+          <input
+            id="name"
+            type="text"
+            value={""}
+            onChange={"your code here"}
+          />
+        </div>
+        <div className="four wide field">
+          <input
+            id="class_year"
+            type="number"
+            value={""}
+            onChange={"your code here"}
+          />
+        </div>
+        <div className="four wide field">
+          <input
+            id="percentage"
+            type="number"
+            value={""}
+            onChange={"your code here"}
+          />
+        </div>
+        <button className="ui button" type="submit">
+          Submit
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export default Student
